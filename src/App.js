9768a50ea51dcdb14a0dd1cbc23751118eb520b3
wilws <wilson.ws.pro@gmail.com>
2022-08-10 15:37:56 +0100

    complete

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import React, { Component } from 'react';
+import lottery from './lottery';
+
+class App extends Component {
+  state = {               // same as the constructor code
+      manager:'',
+      players:[],
+      balance:'',
+      value:'',
+      message:''
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();   // dont need to specific sender account if we use Metamask provider. Will handle by itself. ** for .call() only
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);   // return in wei
+
+
+    this.setState({ manager,players,balance });
+  }
+
+  onSubmit = async (event) =>{
+      event.preventDefault();    
+      const accounts = await web3.eth.getAccounts();
+      this.setState({ message: "Waiting on transaction success..."});
+
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value,'ether')
+      });
+
+      this.setState({ message: 'You havebeen entered!' });
+  };
+
+  onClick = async ()=>{
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'Waiting on transaction success'});
+      await lottery.methods.pickWinner().send({
+        from: accounts[0]
+      });
+    this.setState({ message: 'A Winner has been picked!'});
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This Contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered, 
+          competing to win {web3.utils.fromWei(this.state.balance,'ether')} ether
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange = {event => this.setState({ value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
+          </form>
+          <hr />
+            <h4>Ready to pick a winner?</h4>
+            <button onClick={this.onClick}>Pick a winner!</button>
+          <hr />
+          <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
